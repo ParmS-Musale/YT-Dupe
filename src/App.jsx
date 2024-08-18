89a1df56bc2b839cfad52e7339fdba0943055ee5
ParmS-Musale <musaleparm9541@gmail.com>
2024-08-18 10:32:28 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { useAuth } from "../context/authProvider";
+import { useAuth } from "./context/AuthProvider";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Search from "./components/Search";
